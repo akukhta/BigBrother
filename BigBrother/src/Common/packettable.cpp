a22dffd2bc85f3aafedcbbc5727b5776a39034be
akukhta <electricsix23@gmail.com>
2021-11-15 00:19:19 +0300

    Table

--- BigBrother/src/Common/packettable.cpp
+++ BigBrother/src/Common/packettable.cpp
@@ -1,18 +1,25 @@
 #include "packettable.h"
 
-const QStringList PacketTable::headers{"Text"};
+const QStringList PacketTable::headers{"Ethernet frame", "Protocol type", "Transport type",
+    "Source MAC", "Destination MAC", "Source IP", "Destination IP"};
 
 PacketTable::PacketTable(QTableWidget *table) : table(table)
 {
-    table->setColumnCount(1);
+    table->setColumnCount(7);
     table->setVerticalHeaderLabels(PacketTable::headers);
 }
 
-void PacketTable::addRow(std::string data)
+void PacketTable::addRow(AbstractPacket &&packet)
 {
-    std::unique_lock<std::mutex> lk(tableMutex);
+    //std::unique_lock<std::mutex> lk(tableMutex);
     size_t rowCount = table->rowCount();
     table->setRowCount(rowCount + 1);
     table->insertRow(rowCount);
-    table->setItem(rowCount, 0, new QTableWidgetItem(data.c_str()));
+    table->setItem(rowCount, 0, new QTableWidgetItem(QString::fromStdString(packet.getEthernetHeaderType())));
+    table->setItem(rowCount, 1, new QTableWidgetItem(QString::fromStdString(packet.getProtocolType())));
+    table->setItem(rowCount, 2, new QTableWidgetItem(QString::fromStdString(packet.getTransportType())));
+    table->setItem(rowCount, 3, new QTableWidgetItem(QString::fromStdString(packet.getSourceMAC())));
+    table->setItem(rowCount, 4, new QTableWidgetItem(QString::fromStdString(packet.getDestMAC())));
+    table->setItem(rowCount, 5, new QTableWidgetItem(QString::fromStdString(packet.getSourceIP())));
+    table->setItem(rowCount, 6, new QTableWidgetItem(QString::fromStdString(packet.getDestIP())));
 }
--- BigBrother/src/Common/packettable.h
+++ BigBrother/src/Common/packettable.h
@@ -3,13 +3,14 @@
 #include <memory>
 #include <QTableWidget>
 #include <mutex>
+#include "Net/AbstractPacket.h"
 
 //Packet table
 class PacketTable
 {
 public:
     PacketTable(QTableWidget *table);
-    void addRow(std::string data);
+    void addRow(AbstractPacket &&packet);
 private:
     QTableWidget *table;
     std::mutex tableMutex;
