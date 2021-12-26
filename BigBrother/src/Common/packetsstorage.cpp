94d1fabdb16b92786a5bf706228c0466a2f8c371
akukhta <electricsix23@gmail.com>
2021-12-26 19:26:14 +0300

    MMF has been added

--- BigBrother/src/Common/packetsstorage.cpp
+++ BigBrother/src/Common/packetsstorage.cpp
@@ -1,9 +1,44 @@
 #include "packetsstorage.h"
 
+std::string const PacketsStorage::dataPath = "data";
+
 PacketsStorage::PacketsStorage()
 {
     auto settings = Settings::getInstance();
     maxSize = getSizeInBytes(settings->getSizeType(), settings->getMaxDumpSize());
+
+    fd = open(dataPath.c_str(), O_RDWR | O_CREAT | O_TRUNC);
+
+    if (fd == -1)
+    {
+        throw std::runtime_error("Can`t create data file");
+    }
+
+    if (ftruncate(fd, maxSize) != 0)
+    {
+        throw std::runtime_error("Cannot resize the data file");
+    }
+
+    data = static_cast<unsigned char*>(mmap(NULL, maxSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+
+    if (data == MAP_FAILED)
+    {
+        throw std::runtime_error("File mapping error");
+    }
+
+    data[0] = 0xaa;
+    data[1] = 0xbb;
+    data[2] = 0xcc;
+    data[3] = 0xdd;
+    data[4] = 0xee;
+    data[5] = 0xff;
+}
+
+PacketsStorage::~PacketsStorage()
+{
+    close(fd);
+    munmap(data, maxSize);
+    //Todo: delete the file here
 }
 
 // 0 - byte
--- BigBrother/src/Common/packetsstorage.h
+++ BigBrother/src/Common/packetsstorage.h
@@ -4,6 +4,13 @@
 #include <vector>
 #include <memory>
 #include <cmath>
+#include <filesystem>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include "Common/ViewSettings.h"
 #include "Net/AbstractPacket.h"
 
@@ -11,6 +18,7 @@ class PacketsStorage
 {
 public:
     PacketsStorage();
+    ~PacketsStorage();
     static size_t getSizeInBytes(size_t format, size_t size);
     std::unique_ptr<AbstractPacket> getPacketByIndex(size_t index);
 
@@ -18,4 +26,7 @@ private:
     //Key value structure which stores "Packet's index" - "Packet's offset in binary file"
     std::unordered_map<size_t, size_t> packetsOffset;
     size_t currentOffset = 0, maxSize;
+    std::string static const dataPath;
+    int fd;
+    unsigned char *data;
 };
